0b5f7205f40f215b96e2c36bf676f5c457db1722
Aman tiwari <amantiwari7462@gmail.com>
2025-02-25 13:19:06 +0530

    Questions Properly added

--- src/dsa/Prefix_Sum/NumOfSubarraysWithOddSum.java
+++ src/dsa/Prefix_Sum/NumOfSubarraysWithOddSum.java
@@ -1,5 +1,6 @@
 package dsa.Prefix_Sum;
-//Leetcode Link -> https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/description///Given an array of integers arr, return the number of subarrays with an odd sum. Since the answer can be very large, return it modulo 109 + 7.
+//Leetcode Link -> https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/description/
+// Given an array of integers arr, return the number of subarrays with an odd sum. Since the answer can be very large, return it modulo 109 + 7.
 public class NumOfSubarraysWithOddSum {
 
     public int numOfSubarrays(int[] arr) {
---
+++ src/dsa/Prefix_Sum/subarraysDivByK.java
@@ -0,0 +1,8 @@
+package dsa.Prefix_Sum;
+//Comapy Tags ->  Twilio ✯   Amazon ✯   Facebook ✯   Snapchat ✯
+// Leetcode Link -> https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+//Questions ->Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+//A subarray is a contiguous part of an array.
+
+public class subarraysDivByK {
+}
